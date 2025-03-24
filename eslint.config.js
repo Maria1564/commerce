d060a9065299ee78450e6e8c2ff85e135ed78c66
Maria <salyginamaria156@gmail.com>
2025-03-24 23:06:29 +0300

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,68 @@
+import path from 'path';
+import eslint from '@eslint/js';
+import eslintPluginTypescript from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+
+export default [
+  eslint.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      },
+      globals: {
+      
+        document: 'readonly',
+        window: 'readonly',
+      
+        browser: true,
+        es6: true
+      }
+    },
+    plugins: {
+      '@typescript-eslint': eslintPluginTypescript,
+      import: eslintPluginImport,
+      react: eslintPluginReact,
+      'react-hooks': eslintPluginReactHooks,
+      prettier: eslintPluginPrettier
+    },
+    rules: {
+      'no-console': 'warn',
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          alphabetize: { order: 'asc' },
+          pathGroups: [
+            {
+              pattern: './**/*.scss',
+              group: 'sibling',
+              position: 'after'
+            }
+          ]
+        }
+      ]
+    },
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx', '.scss', '.svg', '.png', '.jpg']
+      },
+      'import/resolver': {
+        typescript: {
+          project: path.resolve('./tsconfig.app.json')
+        }
+      },
+      'import/external-module-folders': ['node_modules', 'node_modules/@types'],
+      react: { version: 'detect' }
+    }
+  },
+  { ignores: ['node_modules'] }
+];
---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfig from "./tsconfig.app.json";
+
+// eslint-disable-next-line no-undef
+const SRC_PATH = path.resolve(__dirname, "src");
+
+const parseTsConfigPaths = (
+  paths: Record<string, string[]>
+): Record<string, string> => {
+  const viteAliases: Record<string, string> = {};
+
+  Object.entries(paths).forEach(([alias, [pathPattern]]) => {
+    const aliasKey = alias.replace(/\/\*$/, "");
+    const pathValue = pathPattern.replace(/\/\*$/, "");
+    const resolvedPath = path.join(SRC_PATH, pathValue);
+    viteAliases[aliasKey] = resolvedPath;
+  });
+
+  return viteAliases;
+};
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: parseTsConfigPaths(tsconfig.compilerOptions.paths),
+  },
+});
