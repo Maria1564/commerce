a1461468b5565d5b8790c5271769f9036ec902c1
Maria <salyginamaria156@gmail.com>
2025-03-29 20:50:48 +0300

    сортировка по новизне

--- src/App/pages/CatalogPage/components/Filter/Dropdown/data.ts
+++ src/App/pages/CatalogPage/components/Filter/Dropdown/data.ts
@@ -10,10 +10,18 @@ export const dataOptions: Option[] = [
   },
   {
     value: "price",
-    text: "сначала недорогие",
+    text: "сначала дешёвые",
   },
   {
     value: "price:desc",
     text: "сначала дорогие",
+  },
+  {
+    value: "publishedAt",
+    text: "сначала старые"
+  },
+  {
+    value: "publishedAt:desc",
+    text: "сначала новые"
   },
 ];
