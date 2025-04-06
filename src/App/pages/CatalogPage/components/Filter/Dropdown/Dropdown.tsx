6ecf51ab89f13dcf3c44f4d5a9dedb1ff95be952
Maria <salyginamaria156@gmail.com>
2025-04-06 15:49:09 +0300

    SortOptionsStore

--- src/App/pages/CatalogPage/components/Filter/Dropdown/Dropdown.tsx
+++ src/App/pages/CatalogPage/components/Filter/Dropdown/Dropdown.tsx
@@ -1,43 +1,34 @@
 import classNames from 'classnames';
 import { observer } from 'mobx-react-lite';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import ArrowDownIcon from 'components/icons/ArrowDownIcon';
 import { useRootStoreContext } from 'store/RootStore/rootStoreProvider';
+import { SortOptionsStore } from 'store/SortOptionsStore/SortOptionsStore';
 import { useClickOutside } from 'utils/hooks/useClickOutside';
+import { useLocalStore } from 'utils/hooks/useLocalStore';
 import { dataOptions, Option } from './data';
 import style from './Dropdown.module.scss';
 
 const Dropdown: React.FC = () => {
-  const [selectOption, setSelectOption] = useState<string>('по популярности');
   const refSelect = useRef<null | HTMLDivElement>(null);
   const { openModal, setOpenModal } = useClickOutside(refSelect);
   const rootStore = useRootStoreContext();
+  const sortStore = useLocalStore(() => new SortOptionsStore())
 
   useEffect(() => {
-    const selectedOption = dataOptions.find((item: Option) => {
-      if (rootStore.queryParams.params.sort) {
-        return item.value === rootStore.queryParams.params.sort;
-      } else {
-        return item.value === '';
-      }
-    });
-
-    if (selectedOption) {
-      setSelectOption(selectedOption.text);
-    }
-  }, [rootStore.queryParams.params]);
+    sortStore.updateSelectedNameOption(rootStore.queryParams.params.sort)
+  }, [rootStore.queryParams.params.sort, sortStore]);
 
   const handleClick = useCallback(() => setOpenModal(!openModal), [openModal, setOpenModal]);
 
   const onSelectOption = useCallback((option: Option) => {
-    setSelectOption(option.text);
     rootStore.queryParams.updateParam('sort', option.value);
-  }, []);
+  }, [rootStore.queryParams]);
 
   return (
     <div className={style.dropdown}>
       <div className={style.dropdown__field} onClick={handleClick} ref={refSelect}>
-        <div className={style[`dropdown__select-item`]}>{selectOption}</div>
+        <div className={style[`dropdown__select-item`]}>{sortStore.selectedNameOption}</div>
         <ArrowDownIcon />
       </div>
       <ul className={classNames(style.dropdown__menu, { [style.dropdown__menu_open]: openModal })}>
@@ -45,7 +36,7 @@ const Dropdown: React.FC = () => {
           <li
             key={index}
             className={classNames(style.dropdown__item, {
-              [style.dropdown__item_active]: item.text === selectOption,
+              [style.dropdown__item_active]: item.text === sortStore.selectedNameOption,
             })}
             onClick={() => onSelectOption(item)}
           >
---
+++ src/store/SortOptionsStore/SortOptionsStore.ts
@@ -0,0 +1,67 @@
+import { action, computed, makeObservable, observable } from 'mobx';
+import { ILocalStore } from 'utils/hooks/useLocalStore';
+
+export type Option = {
+  value: string;
+  text: string;
+};
+
+type PrivateFields =  "_selectOption"
+
+export class SortOptionsStore implements ILocalStore {
+  private _optionslist: Option[] = [
+    {
+      value: '',
+      text: 'по популярности',
+    },
+    {
+      value: 'price',
+      text: 'сначала дешёвые',
+    },
+    {
+      value: 'price:desc',
+      text: 'сначала дорогие',
+    },
+    {
+      value: 'publishedAt',
+      text: 'сначала старые',
+    },
+    {
+      value: 'publishedAt:desc',
+      text: 'сначала новые',
+    },
+  ];
+
+  private _selectOption: string =""
+
+  constructor () {
+    this._selectOption = this._optionslist[0].value
+
+    makeObservable<SortOptionsStore, PrivateFields>(this, {
+        _selectOption: observable,
+        selectedNameOption: computed,
+        updateSelectedNameOption: action
+
+    })
+  }
+
+  get listOptions():Option[] {
+    return this._optionslist
+  }
+
+  get selectedNameOption(): string {
+    return this._selectOption
+  }
+
+  updateSelectedNameOption(selectedOptionValue: string | undefined): void {
+        this._selectOption = this._optionslist.find((item) => {
+            if(selectedOptionValue) {
+                return item.value === selectedOptionValue
+            }else {
+                return item.value === ""
+            }
+        })?.text!
+  }
+
+  destroy() {}
+}
